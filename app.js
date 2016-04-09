0bccb1a42254bdb5b295b7f34f6cea992ccb163f
DusanSacha <sacha.dusan@gmail.com>
2016-04-09 16:41:08 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+var engines = require('consolidate');
+var assert = require('assert');
+
+app.engine('html', engines.nunjucks);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+
+console.log("Successfully connected to server");
+
+app.get('/', function(req,res) {
+	res.render('index');
+});
+
+app.use(function(req,res) {
+	res.sendStatus(404);
+});
+
+var server = app.listen(3000, function() {
+	var port = server.address().port;
+	console.log('Express server listening on port %s', port);
+});
+
