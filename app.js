b8eda25decbc2406447d541286f334726922ed33
DusanSacha <sacha.dusan@gmail.com>
2016-05-12 11:08:11 +0200

    Template files

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 var express = require('express');
 var app = express();
-var engines = require('consolidate');
+var nunjucks = require('nunjucks');
 var assert = require('assert');
+var path = require('path');
 
-app.engine('html', engines.nunjucks);
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
 app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist'));
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+
+nunjucks.configure('views', {
+    autoescape: true,
+    express: app
+});
 
 console.log("Successfully connected to server");
 
